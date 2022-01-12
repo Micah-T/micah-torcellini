b0a822d8e8314d610d8f2554b78ef83328f41700
Micah-T <45294130+Micah-T@users.noreply.github.com>
2022-01-12 08:56:58 -0500

    slimmed down more stuff

--- _11ty/ytEmbed.js
+++ _11ty/ytEmbed.js
@@ -1,7 +1,4 @@
 module.exports = function(i, w, h, t, c) {
-  <div>
-    return  `<lite-youtube videoid="${i}" style="background-image: url('https://i.ytimg.com/vi/${i}/hqdefault.jpg');" class="${c}" title="${t}">
-    </lite-youtube>`
-    <p class="lite-yt-fallback"><a href="https://www.youtube.com/watch?v=${i}">View video on YouTube</a></p>
-  </div>
+    return  `<div><lite-youtube videoid="${i}" style="background-image: url('https://i.ytimg.com/vi/${i}/hqdefault.jpg');" class="${c}" title="${t}">
+    </lite-youtube><p class="lite-yt-fallback"><a href="https://www.youtube.com/watch?v=${i}">View video on YouTube</a></p></div>`
   };
